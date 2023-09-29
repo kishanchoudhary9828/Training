0a8e6cbb128960c3b4b761954cf5d9f40950c7e9
kishan <kishan.choudhary@emails.emizentech.com>
2023-09-29 12:12:56 +0530

    hello

---
+++ resources/views/backend/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="card-footer">
+                <form action="#" method="post">
+                  <div class="input-group">
+                    <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                    <span class="input-group-append">
+                      <button type="button" class="btn btn-primary">Send</button>
+                    </span>
+                  </div>
+                </form>
+              </div>
+              <!-- /.card-footer-->
+            </div>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminLoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+route::get('indexview',[DashboardController::class,'index']);
+
+
+// route::get('loginview',[DashboardController::class,'login']);
